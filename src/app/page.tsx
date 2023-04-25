32ca3eb546efc1f8beca41a0172f998aff88a62f
Marwan Hisham <marwanhisham@Marwans-MacBook-Air.local>
2023-04-25 20:03:28 +0200

    success at publishing a post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
 import LargeHeading from "@/components/common/LargeHeading";
 import Paragraph from "@/components/common/Paragraph";
+import { db } from "@/lib/db";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await db.post.findMany();
+  console.log(posts);
   return (
-    <section className="text-center">
-      <span className="bg-purple-100 text-purple-500 p-2 rounded-lg">
-        Our blog
-      </span>
-      <LargeHeading className="mt-2 mb-4" size="lg">
-        Resources and insights
-      </LargeHeading>
-      <Paragraph>
-        The latest industry news, interviews, technologies, and resources.
-      </Paragraph>
-    </section>
+    <>
+      <section className="text-center">
+        <span className="bg-purple-100 text-purple-500 p-2 rounded-lg">
+          Our blog
+        </span>
+        <LargeHeading className="mt-2 mb-4" size="lg">
+          Resources and insights
+        </LargeHeading>
+        <Paragraph>
+          The latest industry news, interviews, technologies, and resources.
+        </Paragraph>
+      </section>
+      <section></section>
+    </>
   );
 }
--- src/pages/api/posts/create.ts
+++ src/pages/api/posts/create.ts
@@ -1,16 +1,35 @@
-import { CreatePostData } from "@/types";
+import { db } from "@/lib/db";
+import { CreatePostData } from "@/types/api";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<CreatePostData>
 ) {
   const { userId } = getAuth(req);
   if (!userId) {
-    return res.status(401).json("Please Sign in to create a post");
+    return res
+      .status(401)
+      .json({ error: "Please Sign in before", createdPost: null });
   }
+  const prismaUser = await db.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
   const data = req.body;
-  console.log(data);
-  return res.status(200).json({ data });
+  try {
+    const result = await db.post.create({
+      data: {
+        title: data.title,
+        authorId: userId,
+      },
+    });
+    return res.status(200).json({ createdPost: result, error: null });
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ error: "Error Creating new Post", createdPost: null });
+  }
 }
